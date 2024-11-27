de8f1b5d3d978feb1d6295edd849066f85bf97dc
Ryan Laursen <67596484+RLaursen@users.noreply.github.com>
2024-11-27 11:38:50 -0800

    default route for iframe

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,11 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
+import { Route, Redirect } from "wouter";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
+    <Route path="/wizard" component={App} nest/>
+    <Redirect to="/wizard"/>
+  </StrictMode>
 );
