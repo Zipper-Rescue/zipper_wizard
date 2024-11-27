816bff377d0148f364989032025d25e1b5d644e3
Ryan Laursen <67596484+RLaursen@users.noreply.github.com>
2024-11-26 22:22:56 -0800

    Initial mockup

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,55 @@
 import Option from "./wizard/Option";
 import Step from "./wizard/Step";
 
+const CantFix = "I'm sorry, we can't fix that";
+
 function App() {
   return (
-    <Step label={"testttt"}>
+    <Step label={"What kind of zipper do you need to fix?"}>
       <Option
-        path={"/test/"}
+        path={"/two_way/"}
         link={
-          <Step>
-            <Option path={"/test0/"} link={"test0"}>
-              test0
-            </Option>
-            <Option path={"/test1/"} link={"test1"}>
-              test1
+          <Step label={"What's wrong with your zipper?"}>
+            <Option path={"/missing_damaged/"} link={CantFix}>
+              Missing/Damaged Teeth.
             </Option>
           </Step>
         }
       >
-        test
+        Two way separating.
       </Option>
-      <Option path={"/test2/"} link={"test2"}>
-        test2
+      <Option path={"/non_sep/"} link={
+        <Step label={"What's wrong with your zipper?"}>
+          <Option path={"/missing_damaged/"} link= {
+            <Step label={"Is it two-way or one-way?"}>
+              <Option path={"/one_way/"} link={CantFix}>
+                One Way.
+              </Option>
+              <Option path={"/two_way/"} link={
+                <Step label={"Is your zipper damaged in one place or more than one place?"}>
+                  <Option path={"/one_place/"} link={
+                    <div>
+                      Take a Top stop from our outdoor kit and crimp the top like this:<br/>
+                      <img src={'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTDZaN3DMgHm01-6C8oAIuJyc1U2k5DM9Qb1g&s'}/><br/>
+                      Click <a href={"example.com"}>HERE</a> to add a top stop to your cart.
+                    </div>
+                  }>
+                    One Place.
+                  </Option>
+                  <Option path={"/more_than_one/"} link={CantFix}>
+                    More Than One.
+                  </Option>
+                </Step>
+              }>
+                Two Way.
+              </Option>
+            </Step>
+          }>
+            Missing/Damaged Teeth.
+          </Option>
+        </Step>
+      }>
+        Non-separating.
       </Option>
     </Step>
   );
