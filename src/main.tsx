7d73e85cdea58bbe223472d255049907f298c8be
Yona Appletree <hypher@gmail.com>
2025-02-16 09:17:54 -0800

    Add @open-iframe-resizer

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import { Route } from "wouter";
+import "@open-iframe-resizer/core";
 
 const rootElement = document.getElementById("root");
 
--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,19 @@ import * as path from "node:path";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    {
+      name: "configure-response-headers",
+      configureServer: (server) => {
+        server.middlewares.use((_req, res, next) => {
+          res.setHeader("Cross-Origin-Embedder-Policy", "require-corp");
+          res.setHeader("Cross-Origin-Opener-Policy", "same-origin");
+          next();
+        });
+      },
+    },
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
