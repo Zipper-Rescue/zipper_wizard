5ab07cdafbd46b388f930fe9198f9baccd8e1e9b
Yona Appletree <hypher@gmail.com>
2025-02-16 10:34:00 -0800

    scrolling

--- src/components/step-layout.tsx
+++ src/components/step-layout.tsx
@@ -19,7 +19,7 @@ export function StepLayout({
       // scrolling into view.
       setTimeout(() => {
         ref.current?.scrollIntoView({ behavior: "smooth", block: "start" });
-      }, 100);
+      }, 30);
     }
   });
 
